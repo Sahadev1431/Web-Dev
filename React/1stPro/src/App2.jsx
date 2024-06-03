09e73281b55938bd256390a74a1afea54453a795
Sahadev1431 <naisahadev@gmail.com>
2024-06-03 16:39:51 +0530

    1st React

---
+++ React/1stPro/src/App2.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App2() {
+    return (
+        <div>
+            <h1>
+                Fruits
+            </h1>
+            <ul>
+                <li>Apple</li>
+                <li>Banana</li>
+                <li>Mango</li>
+            </ul>
+        </div>
+    )
+}
---
+++ React/1stPro/src/main.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+//here importing various App
+import App2 from "./App2.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App2 />);
